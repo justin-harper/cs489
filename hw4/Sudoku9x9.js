65fe3badb7e6d61fb6fbdcee18f8f9af7d8b5cee
jharper <justin.harper@wsu.edu>
2017-03-07 16:19:02 -0800

    Init classes

--- hw4/Sudoku9x9.js
+++ hw4/Sudoku9x9.js
@@ -1,4 +1,160 @@
-// Student name: (put your name here)
-// Student ID: (put your ID number here)
+// Student name: Justin Harper
+// Student ID: 10696738
 
-// your code here
+
+function Sudoku9x9(arrOf81Values)
+{
+
+}
+
+Sudoku9x9.prototype.get3x3 = function(rowIndex, collIndex)
+{
+
+};
+
+Sudoku9x9.prototype.getColumn = function(collIndex)
+{
+
+};
+
+Sudoku9x9.prototype.getRow = function(rowIndex)
+{
+
+};
+
+Sudoku9x9.prototype.toArray = function()
+{
+
+};
+
+
+
+function SudokuCell(numPossibleValues)
+{
+    this.finalizedValue = null;
+}
+
+SudokuCell.prototype.containsPossibliity = function(value)
+{
+
+};
+
+SudokuCell.prototype.getPossibilities = function()
+{
+
+};
+
+SudokuCell.prototype.isFinalized = function()
+{
+
+};
+
+SudokuCell.prototype.removePossibility = function(value)
+{
+
+};
+
+SudokuCell.prototype.removePossibilities = function()
+{
+
+};
+
+SudokuCell.prototype.toString = function()
+{
+
+};
+
+
+function SudokuCellCollection(arrOfCells)
+{
+  var length;
+}
+
+SudokuCellCollection.prototype.containsCell = function(value)
+{
+
+};
+
+SudokuCellCollection.prototype.containsPossibliity = function(value)
+{
+
+};
+
+SudokuCellCollection.prototype.count = function(predicate)
+{
+
+};
+
+SudokuCellCollection.prototype.forEach = function(functionThatTakes1CellParam, startIndex)
+{
+
+};
+
+SudokuCellCollection.prototype.getFinalizedValues = function()
+{
+
+};
+
+SudokuCellCollection.prototype.getPossibilities = function()
+{
+
+};
+
+SudokuCellCollection.prototype.removeCell = function(cell)
+{
+
+};
+
+SudokuCellCollection.prototype.removeCells = function(otherCellCollection)
+{
+
+};
+
+SudokuCellCollection.prototype.removePossibility = function(value)
+{
+
+};
+
+
+function SudokuCellBlock(arrOf9Values)
+{
+  SudokuCellCollection.call(this, arrOf9Values);
+}
+SudokuCellBlock.prototype = Object.create(SudokuCellCollection.prototype);
+
+SudokuCellBlock.prototype.trySolve = function()
+{
+
+};
+
+
+function Sudoku3x3Block(arrOf9Values)
+{
+  SudokuCellBlock.call(this, arrOf9Values)
+}
+Sudoku3x3Block.prototype = Object.create(SudokuCellBlock.prototype);
+
+Sudoku3x3Block.prototype.getPossibilitiesOnlyAvailableOnColumn = function(collIndex)
+{
+
+};
+
+Sudoku3x3Block.prototype.getPossibilitiesOnlyAvailableOnRow = function(rowIndex)
+{
+
+};
+
+Sudoku3x3Block.prototype.isColumnFinalized = function(columnIndex)
+{
+
+};
+
+Sudoku3x3Block.prototype.isRowFinalized = function(rowIndex)
+{
+
+};
+
+Sudoku3x3Block.prototype.toString = function()
+{
+
+};
